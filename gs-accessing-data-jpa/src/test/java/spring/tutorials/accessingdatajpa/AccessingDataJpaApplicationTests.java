ff7838fc868d9f3d7840580c26a35fadb2bb5d56
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-02 18:50:50 +0900

    Initialize project

---
+++ gs-accessing-data-jpa/src/test/java/spring/tutorials/accessingdatajpa/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.tutorials.accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
