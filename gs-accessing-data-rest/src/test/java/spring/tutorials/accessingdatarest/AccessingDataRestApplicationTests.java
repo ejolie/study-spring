1460dde15ed1b064c395b71760e020020d52704a
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-03 12:57:52 +0900

    Initialize project

---
+++ gs-accessing-data-rest/src/test/java/spring/tutorials/accessingdatarest/AccessingDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.tutorials.accessingdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
