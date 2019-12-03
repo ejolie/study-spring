9b1f54ff4f5b688c546c2f3f75e2d2c5e8b07413
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-03 12:57:34 +0900

    Complete tutorial

---
+++ gs-accessing-data-jpa/src/main/java/spring/tutorials/accessingdatajpa/CustomerRepository.java
@@ -0,0 +1,12 @@
+package spring.tutorials.accessingdatajpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
