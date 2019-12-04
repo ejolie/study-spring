983c7f1dfb03a4d03cad6ddda0baa14662708a3a
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-04 17:21:27 +0900

    Complete 스프링 REST 클라이언트 1부: RestTemplate과 WebClient

--- 스프링-부트-개념과-활용/springbootrest/src/main/java/com/example/springbootrest/FluxRunner.java
+++ 스프링-부트-개념과-활용/springbootrest/src/main/java/com/example/springbootrest/FluxRunner.java
@@ -1,4 +1,60 @@
 package com.example.springbootrest;
 
-public class FluxRunner {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class FluxRunner implements ApplicationRunner {
+
+    @Autowired
+    WebClient.Builder builder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        WebClient webClient = builder.build();
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        // TODO /hello
+        Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        // subscribe 하기 전까진 아무 일도 일어나지 않음
+        // 2. 5초 후 출력 (world 출력 + 2초 후)
+        helloMono.subscribe(s -> {
+            System.out.println(s);
+
+            if (stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
+
+        // TODO /world
+        Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        // 1. 3초 후 출력
+        worldMono.subscribe(s -> {
+            System.out.println(s);
+
+            if (stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start();
+        });
+    }
 }
