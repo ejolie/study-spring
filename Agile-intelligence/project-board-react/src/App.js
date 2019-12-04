8d064b68e8a5b6579cdffa375bb83110c0be7d1b
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-04 18:57:55 +0900

    Complete 07. ReactJS: Set up project

---
+++ Agile-intelligence/project-board-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='alert alert-warning'>Welcome to the project board</h1>
+    </div>
+  );
+}
+
+export default App;
