ad55b4541d291dfb010d874f5d8126cf6d82451d
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-05 16:39:12 +0900

    Add spring-security-jpa project

---
+++ Java-brains/spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package io.javabrains.springsecurityjpa;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+}
