751041d46429f92fa5722be4ce40b5a9793d1366
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-17 16:38:28 +0900

    Complete spring-security-jpa

--- Java-brains/spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SecurityConfiguration.java
+++ Java-brains/spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SecurityConfiguration.java
@@ -1,13 +1,24 @@
 package io.javabrains.springsecurityjpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
@@ -17,5 +28,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
+    }
+
+    // Not Recommanded
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- Java-brains/spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplication.java
+++ Java-brains/spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -2,8 +2,10 @@ package io.javabrains.springsecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringSecurityJpaApplication {
 
 	public static void main(String[] args) {
