f04b98fa37eeaeabfe1d28326558078da9779a4d
ejolie <ejolie.dev@gmail.com>
2020-03-15 19:22:46 +0900

    Complete 1부. DispatcherServlet 2부

--- 스프링-웹-MVC/java-servlet-demo/src/main/java/org/example/HelloController.java
+++ 스프링-웹-MVC/java-servlet-demo/src/main/java/org/example/HelloController.java
@@ -1,17 +1,24 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello, " + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "/WEB-INF/sample.jsp";
     }
 }
---
+++ 스프링-웹-MVC/java-servlet-demo/src/main/java/org/example/SimpleController.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@org.springframework.stereotype.Controller("/simple")
+public class SimpleController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        return new ModelAndView("/WEB-INF/simple.jsp");
+    }
+}
