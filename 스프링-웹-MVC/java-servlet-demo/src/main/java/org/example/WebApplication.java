c41235ec603a43d14591e7b79071e441709121fd
ejolie <ejolie.dev@gmail.com>
2020-03-15 23:29:10 +0900

    Complete 1부. 스프링 MVC 동작 원리 마무리

---
+++ 스프링-웹-MVC/java-servlet-demo/src/main/java/org/example/WebApplication.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+// 스프링 부트 X 스프링 MVC DispatcherServlet 등록 방법
+// 2. WebApplicationInitializer에 자바 코드로 서블릿 등록
+public class WebApplication implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.refresh();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic app = servletContext.addServlet("app", dispatcherServlet);
+        app.addMapping("/app/*");
+    }
+}
