14467a88d9bc813b76c2be974e8b4001394a8901
ejolie <ejolie.dev@gmail.com>
2020-03-26 02:36:59 +0900

    Complete 3부. 요청 맵핑하기 3부 미디어 타입

--- 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/SampleController.java
+++ 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/SampleController.java
@@ -1,20 +1,18 @@
 package com.example.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
     // 컨트롤러 안의 메서드: 핸들러 메서드
-    @GetMapping("/keesun")
-    @ResponseBody
-    public String helloKeesun() {
-        return "hello keesun";
-    }
-
-    @GetMapping("/**")
+    @GetMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
     @ResponseBody
     public String hello() {
         return "hello";
