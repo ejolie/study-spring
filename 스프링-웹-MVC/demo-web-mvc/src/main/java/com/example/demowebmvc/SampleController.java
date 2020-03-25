0c31df12e793f63a0d9850129cdd64e9b63c2930
ejolie <ejolie.dev@gmail.com>
2020-03-26 02:50:26 +0900

    Complete 3부. 요청 맵핑하기 4부 헤더와 매개변수

--- 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/SampleController.java
+++ 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/SampleController.java
@@ -1,6 +1,5 @@
 package com.example.demowebmvc;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class SampleController {
 
     // 컨트롤러 안의 메서드: 핸들러 메서드
-    @GetMapping(
-            value = "/hello",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.TEXT_PLAIN_VALUE
-    )
+    @GetMapping(value = "/hello", params = "name=keesun")
     @ResponseBody
     public String hello() {
         return "hello";
--- 스프링-웹-MVC/demo-web-mvc/src/test/java/com/example/demowebmvc/SampleControllerTest.java
+++ 스프링-웹-MVC/demo-web-mvc/src/test/java/com/example/demowebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,7 +23,7 @@ class SampleControllerTest {
     @Test
     public void helloTest() throws Exception {
         mockMvc.perform(get("/hello")
-                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .param("name", "keesun")
         )
                 .andDo(print())
                 .andExpect(status().isOk());
