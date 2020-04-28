9360450c075621cd6cf366ba31f81785da3819b9
ejolie <ejolie.dev@gmail.com>
2020-04-29 01:43:08 +0900

    Complete 3부. 핸들러 메소드 2부 URI 패턴

--- 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/SampleController.java
+++ 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/SampleController.java
@@ -1,35 +1,20 @@
 package com.example.demowebmvc;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class SampleController {
-
-    @GetMapping("/events")
-    @ResponseBody
-    public String getEvents() {
-        return "events";
-    }
 
     @GetMapping("/events/{id}")
     @ResponseBody
-    public String getEvent(@PathVariable int id) {
-        return "event";
-    }
-
-    @DeleteMapping("/events/{id}")
-    @ResponseBody
-    public String deleteEvent(@PathVariable int id) {
-        return "deleted";
-    }
-
-    // 컨트롤러 안의 메서드: 핸들러 메서드
-    @GetHelloMapping
-    @ResponseBody
-    public String hello() {
-        return "hello";
+    public Event getEvent(@PathVariable Integer id, @MatrixVariable String name) {
+        Event event = new Event();
+        event.setId(id);
+        event.setName(name);
+        return event;
     }
 }
--- 스프링-웹-MVC/demo-web-mvc/src/test/java/com/example/demowebmvc/SampleControllerTest.java
+++ 스프링-웹-MVC/demo-web-mvc/src/test/java/com/example/demowebmvc/SampleControllerTest.java
@@ -1,11 +1,9 @@
 package com.example.demowebmvc;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,55 +19,11 @@ class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void helloTest() throws Exception {
-        mockMvc.perform(options("/hello"))
+    public void getEvent() throws Exception {
+        mockMvc.perform(get("/events/1;name=keesun"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1));
     }
 
-    @Test
-    public void getEvents() throws Exception {
-        mockMvc.perform(get("/events"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void getEventsWithId() throws Exception {
-        mockMvc.perform(get("/events/1"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/events/2"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/events/3"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void createEvent() throws Exception {
-        mockMvc.perform(post("/events")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void deleteEvent() throws Exception {
-        mockMvc.perform(delete("/events/1"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(delete("/events/2"))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(delete("/events/3"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void updateEvent() throws Exception {
-        mockMvc.perform(put("/events/1")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk());
-    }
 }
