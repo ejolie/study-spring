db6ff3d0a859376abf78d301ebd8beb55b7db899
ejolie <ejolie.dev@gmail.com>
2020-05-04 00:37:00 +0900

    Complete 2부. 핸들러 인터셉터

--- 스프링-웹-MVC/demo-boot-web/src/main/java/com/example/demobootweb/SampleController.java
+++ 스프링-웹-MVC/demo-boot-web/src/main/java/com/example/demobootweb/SampleController.java
@@ -7,6 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    /**
+     * Handler Interceptor
+     * -> preHandle 1
+     * -> preHandle 2
+     * -> 요청 처리
+     * -> postHandle 2
+     * -> postHandle 1
+     * -> 뷰 렌더링
+     * -> afterCompletion 2
+     * -> afterCompletion 1
+     */
+
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
         return "hello " + person.getName();
