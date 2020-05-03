866516ff49db5ab385703df302f46060eaaff99e
ejolie <ejolie.dev@gmail.com>
2020-05-04 00:45:24 +0900

    Complete 2부. 핸들러 인터셉터 구현

--- 스프링-웹-MVC/demo-boot-web/src/main/java/com/example/demobootweb/WebConfig.java
+++ 스프링-웹-MVC/demo-boot-web/src/main/java/com/example/demobootweb/WebConfig.java
@@ -1,14 +1,18 @@
 package com.example.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addFormatters(FormatterRegistry registry) {
-//        registry.addFormatter(new PersonFormatter());
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // order 숫자가 낮을수록 우선순위가 높다
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(-1);
+    }
 }
