6d5f39c24d75e965d442db3a3e5b0a916888ce75
ejolie <ejolie.dev@gmail.com>
2020-05-05 21:14:34 +0900

    Complete 2부. HTTP 메시지 컨버터

--- 스프링-웹-MVC/demo-boot-web/src/main/java/com/example/demobootweb/SampleController.java
+++ 스프링-웹-MVC/demo-boot-web/src/main/java/com/example/demobootweb/SampleController.java
@@ -1,6 +1,7 @@
 package com.example.demobootweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,9 @@ public class SampleController {
         return "hello " + person.getName();
     }
 
+    @GetMapping("/message")
+    // @ResponseBody
+    public String message(@RequestBody String body) {
+        return body;
+    }
 }
--- 스프링-웹-MVC/demo-boot-web/src/test/java/com/example/demobootweb/SampleControllerTest.java
+++ 스프링-웹-MVC/demo-boot-web/src/test/java/com/example/demobootweb/SampleControllerTest.java
@@ -5,17 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -46,5 +43,14 @@ class SampleControllerTest {
                 .andDo(print())
                 .andExpect(content().string(Matchers.containsString("Hello Mobile")))
                 .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+    }
+
+    @Test
+    public void stringMessage() throws Exception {
+        mockMvc.perform(get("/message")
+                .content("hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
     }
 }
