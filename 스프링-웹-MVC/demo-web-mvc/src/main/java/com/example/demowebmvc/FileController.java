724e1f35e84f1dadc80428c74286b2742f5e8813
ejolie <ejolie.dev@gmail.com>
2020-05-16 16:21:15 +0900

    Complete 3부. 핸들러 메소드 14부 ResponseEntity

--- 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/FileController.java
+++ 스프링-웹-MVC/demo-web-mvc/src/main/java/com/example/demowebmvc/FileController.java
@@ -1,15 +1,24 @@
 package com.example.demowebmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model) {
@@ -23,5 +32,23 @@ public class FileController {
         String message = file.getOriginalFilename() + " is uploaded";
         redirectAttributes.addFlashAttribute("message", message);
         return "redirect:/file";
+    }
+
+    @GetMapping("/file/{filename}")
+    @ResponseBody
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+
+        Tika tika = new Tika();
+        String mediaType = tika.detact(file);
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, mediaType)
+                .header(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length()))
+                .body(resource);
+
     }
 }
