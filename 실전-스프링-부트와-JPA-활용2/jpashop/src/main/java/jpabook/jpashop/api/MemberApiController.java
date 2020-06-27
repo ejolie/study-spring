0d9ec5400868a39802d94f937343252f2104f125
ejolie <ejolie.dev@gmail.com>
2020-06-27 13:57:52 +0900

    Complete API 개발 기본 - 회원 조회 API

--- 실전-스프링-부트와-JPA-활용2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ 실전-스프링-부트와-JPA-활용2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,12 +8,47 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    /**
+     * - Entity에 @JsonIgnore를 붙이는 것은 만능 해결책이 아니다.
+     * - Entity에 화면을 위한 로직, Presentation 레벨이 들어와선 안된다.
+     * - Entity가 바뀌면 API 스펙도 바꿔야 하는 문제가 생긴다.
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> memberDtos = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(memberDtos.size(), memberDtos);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     /**
      * API를 만들 때 주의할 점
