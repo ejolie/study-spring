dfb48cab8d67a3d4236f6aa6d7bc3ef6376cd12e
ejolie <ejolie.dev@gmail.com>
2020-06-27 15:41:17 +0900

    Complete API 개발 고급: 지연 로딩과 조회 성능 최적화 - 간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- 실전-스프링-부트와-JPA-활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ 실전-스프링-부트와-JPA-활용2/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -27,10 +27,10 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
     /**
-     * 문제
-     * 1. 양방향 연관 관계로 무한 루프 발생
+     * V1. Entity를 직접 노출
+     * - 양방향 연관 관계로 무한 루프 발생
      *   -> 한 쪽에는 @JsonIgnore 써줘야 함
-     * 2. 지연 로딩(fetch = LAZY) 문제
+     * - 지연 로딩(fetch = LAZY) 문제
      *   - Order에서 프록시 맴버 객체로 갖고 있다가 나중에 진짜 멤버 객체 정보 가져옴
      *   -> Jackson DataType Hibernate5 모듈 추가
      */
@@ -44,9 +44,36 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    /**
+     * V2. 엔티티를 DTO로 변환
+     * -> N + 1 문제
+     * - 쿼리가 총 1 + N + N번 실행된다. (v1와 쿼리수 결과는 같다.)
+     *  - ORDER 조회 1번 -> 조회 결과 수 N
+     *  - ORDER -> MEMBER 지연 로딩 조회 N
+     *  - ORDER -> DELIVERY 지연 로딩 조회 N
+     *
+     * -> 즉, ORDER 조회 결과가 2개면 최악의 경우 1 + 2 + 2번 실행된다.
+     *  - 지연 로딩은 기본적으로 N번이긴 한데 DB 쿼리로 조회하는 것이 아니라
+     *    영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.
+     */
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    /**
+     * V3. 엔티티를 DTO로 변환 + Fetch Join으로 성능 최적화
+     * - 쿼리 1번 실행
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
 
         List<SimpleOrderDto> result = orders.stream()
                 .map(SimpleOrderDto::new)
@@ -71,4 +98,5 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress(); // LAZY 초기화
         }
     }
+
 }
