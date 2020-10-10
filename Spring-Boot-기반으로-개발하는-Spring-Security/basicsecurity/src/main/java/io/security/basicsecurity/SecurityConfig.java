0b7f9a11caa2fcd7308f5c8b83ba4923163ba8b9
ejolie <ejolie.dev@gmail.com>
2020-10-10 16:35:50 +0900

    스프링 시큐리티 기본 API 및 Filter 이해 - 6. Remember Me 인증

--- Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,15 +1,20 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.servlet.http.HttpSession;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated();
         // 2. 인증
-        // - 로그인
+        // 로그인
         http
                 .formLogin()
 //            .loginPage("/loginPage")
@@ -26,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login")
                 .usernameParameter("userId")
                 .passwordParameter("userPwd")
-                .loginProcessingUrl("/login_proc")
+                .loginProcessingUrl("/login")
                 .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
                     System.out.println("authentication : " + authentication.getName());
                     httpServletResponse.sendRedirect("/");
@@ -37,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .permitAll(); // loginPage에는 누구나 접근가능하게 만듦
 
-        // - 로그아웃
+        // 로그아웃
         http
                 .logout()
                 .logoutUrl("/logout")
@@ -50,5 +55,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     httpServletResponse.sendRedirect("/login");
                 })
                 .deleteCookies("remember-me");
+
+        // Remember-me
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600);
+//                .alwaysRemember(true)
+//                .userDetailsService(userDetailsService);
     }
 }
