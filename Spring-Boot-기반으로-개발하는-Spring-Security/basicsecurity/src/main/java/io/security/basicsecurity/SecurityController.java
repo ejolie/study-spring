0fbfb1e44dd0ab2dd00ce2cb50f1c7e7b93ff290
ejolie <ejolie.dev@gmail.com>
2020-10-10 15:14:29 +0900

    스프링 시큐리티 기본 API 및 Filter 이해 - 1. 프로젝트 구성 및 의존성 추가

---
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -0,0 +1,18 @@
+package io.security.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+}
