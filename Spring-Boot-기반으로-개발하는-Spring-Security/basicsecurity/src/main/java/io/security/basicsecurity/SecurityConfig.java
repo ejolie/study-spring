19a302a46636db20f3af12285292fa7ca62df03f
ejolie <ejolie.dev@gmail.com>
2020-10-10 16:23:26 +0900

    스프링 시큐리티 기본 API 및 Filter 이해 - 5. Logout 처리, LogoutFilter

--- Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,15 +5,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.http.HttpSession;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 1. 인가
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
+        // 2. 인증
+        // - 로그인
         http
                 .formLogin()
 //            .loginPage("/loginPage")
@@ -31,5 +36,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     httpServletResponse.sendRedirect("/login");
                 })
                 .permitAll(); // loginPage에는 누구나 접근가능하게 만듦
+
+        // - 로그아웃
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    HttpSession session = httpServletRequest.getSession();
+                    session.invalidate();
+                })
+                .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    httpServletResponse.sendRedirect("/login");
+                })
+                .deleteCookies("remember-me");
     }
 }
