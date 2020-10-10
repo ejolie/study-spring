e97934484a70a3dc0e6cf4421782f604b6e95d4d
ejolie <ejolie.dev@gmail.com>
2020-10-10 23:46:20 +0900

    스프링 시큐리티 기본 API 및 Filter 이해 - 11. 권한설정과 표현식

--- Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,11 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+        // Role Hierarchy 기능을 활용한다면 모든 권한을 부여하지 않아도 ADMIN 역할만으로 충분함
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 1. 인가
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
+
         // 2. 인증
         // 로그인
         http
--- Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -11,8 +11,18 @@ public class SecurityController {
         return "home";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "adminPay";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
     }
 }
