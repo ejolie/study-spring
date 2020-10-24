84626630ca96bd9e7b1d80384e0930c1116809be
ejolie <ejolie.dev@gmail.com>
2020-10-24 16:45:32 +0900

    스프링 시큐리티 주요 아키텍처 이해 - 4. 인증 저장소 - SecurityContextHolder, SecurityContext

--- Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfigMulti.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfigMulti.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Order(0)
 @Configuration
@@ -19,9 +20,13 @@ public class SecurityConfigMulti extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
 
+// Order(0) 으로 하면 /admin 접속 시 로그인 필요 없어짐
+// 따라서, 구체적인 URL 범위를 먼저 처리하도록 우선순위를 높여야 함
 @Order(1)
 @Configuration
 class SecurityConfig2 extends WebSecurityConfigurerAdapter {
--- Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,14 +1,38 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession httpSession) {
+        // 1. SecurityContextHolder 에서 인증 객체 가져오기
+        Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+
+        // 2. (인증 후) HttpSession 에서 인증 객체 가져오기
+        SecurityContext securityContext = (SecurityContext) httpSession.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication2 = securityContext.getAuthentication();
+
         return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+        // 자식 스레드
+        // - 기본값인 MODE_THREADLOCAL 전략에서는 메인 스레드와 컨텍스트를 공유하지 않음
+        new Thread(() -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        }).start();
+
+        return "thread";
     }
 
     @GetMapping("/user")
