c98d1513037ff7a87a0b63cbd64c0fd9f757e21b
ejolie <ejolie.dev@gmail.com>
2020-11-09 16:18:43 +0900

    실전 프로젝트: 인증 프로세스 Form 인증 구현 - 2. 정적 자원 관리 - WebIgnore 설정

--- Spring-Boot-기반으로-개발하는-Spring-Security/corespringsecurity/src/main/java/com/ejolie/corespringsecurity/security/configs/SecurityConfig.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/corespringsecurity/src/main/java/com/ejolie/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.ejolie.corespringsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -18,13 +20,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         String password = passwordEncoder().encode("1111");
 
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        // permitAll() 과 차이점: 보안 필터 자체를 거치지 않음, 보안 필터 범위 밖
     }
 
     @Override
