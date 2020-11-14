e7a449da952ffc8c98008b19c55a79397e81e844
ejolie <ejolie.dev@gmail.com>
2020-11-15 03:48:08 +0900

    실전 프로젝트: 인증 프로세스 Form 인증 구현 - 7. 로그아웃 및 인증에 따른 화면 보안 처리

--- Spring-Boot-기반으로-개발하는-Spring-Security/corespringsecurity/src/main/java/com/ejolie/corespringsecurity/controller/login/Login.java
+++ Spring-Boot-기반으로-개발하는-Spring-Security/corespringsecurity/src/main/java/com/ejolie/corespringsecurity/controller/login/Login.java
@@ -1,7 +1,13 @@
 package com.ejolie.corespringsecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class Login {
@@ -9,5 +15,15 @@ public class Login {
     @GetMapping("/login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login";
     }
 }
