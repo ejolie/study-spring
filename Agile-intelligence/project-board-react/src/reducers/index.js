388b0082c1e8359600246b8091fa795e55ca6e03
ejolie <ejolie.dev@gmail.com>
2021-02-20 20:37:29 +0900

    Apply .gitignore and remove cached

--- Agile-intelligence/project-board-react/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import errorsReducer from './errorsReducer';
-import projectTaskReducer from './projectTaskReducer';
-
-export default combineReducers({
-  errors: errorsReducer,
-  project_task: projectTaskReducer
-});
